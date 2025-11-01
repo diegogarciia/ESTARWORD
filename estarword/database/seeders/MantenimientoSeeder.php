54b5c224c5c03d9b650a72d14c311d958ae96eb7
Diego <diegogarciaangel25@gmail.com>
2025-11-01 10:19:02 +0100

    faltaba el seeder de mantenimiento

--- estarword/database/seeders/MantenimientoSeeder.php
+++ estarword/database/seeders/MantenimientoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Mantenimiento;
 
 class MantenimientoSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class MantenimientoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Mantenimiento::factory()->count(20)->create();
     }
 }
